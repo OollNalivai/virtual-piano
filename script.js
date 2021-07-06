8aa46a0a04e6db18b99c30d38a23ad47dfa04dde
VladislavOoll <apahiphip@mail.ru>
2021-07-06 23:45:23 +0300

    refactoring: switch actove mode, event keypress, break line, active event, remove let flag, const play

--- script.js
+++ script.js
@@ -1,70 +1,77 @@
-const btn_notes = document.querySelector('.btn-notes');
-const btn_letters = document.querySelector('.btn-letters');
-const arr = document.querySelectorAll('.piano-key');
+const btnNotes = document.querySelector('.btn-notes');
+const btnLetters = document.querySelector('.btn-letters');
+const keyArray = document.querySelectorAll('.piano-key');
 const piano = document.querySelector('.piano');
-let flag = true;
 
 // play Audio
 const elem = async (src) => {
     const audio = new Audio();
     if (src) {
         audio.src = src;
+
         await audio.play();
     }
 }
 
-// btn switch notes mode
-btn_notes.addEventListener('click', () => {
-    btn_notes.classList.add('btn-active');
-    btn_letters.classList.remove('btn-active');
-    for (let elem of arr) {
-        elem.classList.remove('piano-key-letter');
-    }
-    flag = true;
+// switch active mode
+const clickEvt = flag => {
     play(flag);
-});
+
+    const notesClassList = btnNotes.classList;
+    const lettersClassList = btnLetters.classList;
+
+    flag ? notesClassList.add('btn-active') : notesClassList.remove('btn-active');
+    flag ? lettersClassList.remove('btn-active') : lettersClassList.add('btn-active');
+
+    for (const {classList} of keyArray) {
+        flag ? classList.remove('piano-key-letter') : classList.add('piano-key-letter');
+    }
+}
+
+// btn switch notes mode
+btnNotes.addEventListener('click', () => clickEvt(true));
 
 // btn switch letters mode
-btn_letters.addEventListener('click', () => {
-    btn_letters.classList.add('btn-active');
-    btn_notes.classList.remove('btn-active');
-    for (let elem of arr) {
-        elem.classList.add('piano-key-letter');
-    }
-    flag = false;
-    play(flag);
-});
+btnLetters.addEventListener('click', () => clickEvt(false));
 
 // event Keypress note
 const eventKeypress = async Event => {
-    const key_pick = [...document.querySelectorAll('.piano-key')]
+    const keyPick = [...document.querySelectorAll('.piano-key')]
         .find(el => 'Key' + el.getAttribute('data-letter') === Event.code);
-    if (key_pick !== undefined) {
-        const src = key_pick?.getAttribute('data-src');
-        key_pick?.classList.add('piano-key-active', 'piano-key-active-pseudo');
+
+    if (Event.repeat) { return }
+
+    if (keyPick !== undefined) {
+        const src = keyPick?.getAttribute('data-src');
+
+        keyPick?.classList.add('piano-key-active', 'piano-key-active-pseudo');
+
         await elem(src);
+
         setTimeout(() => {
-            arr.forEach(element => {
+            keyArray.forEach(element => {
                 if (element.classList.contains('piano-key-active')) {
-                    key_pick.classList
+                    keyPick.classList
                         .remove('piano-key-active', 'piano-key-active-pseudo');
+
                 }
             });
         }, 70);
     }
 }
 
-// const createEvent = (piano, event, callback) => piano.addEventListener(event, callback);
 // event Click Mouseover note
 const eventClick = async Event => {
     const target = Event.target;
     target.classList
         .add('piano-key-active', 'piano-key-active-pseudo');
+
     const src = target.getAttribute('data-src');
+
     await elem(src);
 
     setTimeout(() => {
-        arr.forEach(el => {
+        keyArray.forEach(el => {
             if (el.classList.contains('piano-key-active')) {
                 target.classList
                     .remove('piano-key-active', 'piano-key-active-pseudo');
@@ -72,6 +79,7 @@ const eventClick = async Event => {
         });
     }, 70);
 }
+
 // event Click Mouseover note
 const eventMouseover = async Event => {
     if (Event.buttons) {
@@ -79,10 +87,11 @@ const eventMouseover = async Event => {
         target.classList
             .add('piano-key-active', 'piano-key-active-pseudo');
         const src = target.getAttribute('data-src');
+
         await elem(src);
 
         setTimeout(() => {
-            arr.forEach(el => {
+            keyArray.forEach(el => {
                 if (el.classList.contains('piano-key-active')) {
                     target.classList
                         .remove('piano-key-active', 'piano-key-active-pseudo');
@@ -92,28 +101,29 @@ const eventMouseover = async Event => {
     }
 };
 
-// click mouseover
-const clickPlay = () => {
-    document.removeEventListener("keypress", eventKeypress);
+// active event
+const evtPlay = flag => {
+    const pianoAddEventListener = piano.addEventListener;
+    const pianoRemoveEventListener = piano.removeEventListener;
 
-    piano.addEventListener('click', eventClick);
-    piano.addEventListener('mouseover', eventMouseover);
+    flag ? document.removeEventListener("keydown", eventKeypress)
+        : document.addEventListener("keydown", eventKeypress);
+
+    flag ? pianoAddEventListener('click', eventClick)
+        : pianoRemoveEventListener('click', eventClick);
+
+    flag ? pianoAddEventListener('mouseover', eventMouseover)
+        : pianoRemoveEventListener('mouseover', eventMouseover);
 }
 
-// keypress
-const pressPlay = () => {
-    document.addEventListener("keypress", eventKeypress);
+evtPlay(true);
 
-    piano.removeEventListener('click', eventClick);
-    piano.removeEventListener('mouseover', eventMouseover);
-}
-
-clickPlay(flag);
-const play = flag => flag ? clickPlay() : pressPlay();
+const play = flag => evtPlay(flag);
 
 // fullscreen
-const fs = document.querySelector('.fullscreen');
-fs.addEventListener('click', () => {
+const fullScreen = document.querySelector('.fullscreen');
+
+fullScreen.addEventListener('click', () => {
     !document.fullscreenElement
         ? document.documentElement.requestFullscreen()
         : document.exitFullscreen()
